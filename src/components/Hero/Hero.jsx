53f793a636839ecf84d859264faaf0bcb5efd8f2
Kartik Prakash <kartikprakash1@gmail.com>
2025-03-31 21:24:25 +0530

    Resume link updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p>I'm a skilled fullstack developer with expertise in React, HTML, Express, MongoDB, and Node.js.</p>
             <div className='hero-action'>
                 <AnchorLink className='anchor-link-hero' offset={50} href='#contact'><div className="hero-connect">Connect with me</div></AnchorLink>
-                <a className='download' href="https://drive.google.com/uc?export=download&id=1YkbNGnMBN1Aa7X6ljLwFwaDl6wUinmHa"><div className="hero-resume">My résumé</div></a>
+                <a className='download' href="https://drive.google.com/file/d/1-EIv_CG_5mBE9z-FsW8FtTQIbMkHXvPd/view?usp=share_link"><div className="hero-resume">My résumé</div></a>
             </div>
         </div>
     )
